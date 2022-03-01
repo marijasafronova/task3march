ef174e3ab4b53bd85f6c24f62db16f0201279730
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-03-01 21:30:44 +0200

    Added some comments

--- src/main/scala/Day2Variables.scala
+++ src/main/scala/Day2Variables.scala
@@ -1,3 +1,20 @@
 object Day2Variables extends App {
-  println("Let's explore some variables")
+  println("Let's explore some variables") //we are printing is not very useful comment right ? :)
+  //we want to store some data while we work
+  //for that we need variables
+  //but first these are comments
+  //comments are only for people not for computers
+  /* this is multi
+    line comment
+    those are used less usually for documentation with extra symbols,
+   */
+  //when you are learning it is fine to make extra comments
+  //one problem with comments is that they can fail to keep up with the code
+  //that is the worst
+  //comment says one thing
+  //code says something else
+  //and you are trying to figure out which one is right
+  //then version control might help
+  //even worse then you realize that it was you who wrote it 6 months ago :)
+  //our human brains can only keep 6-8 concepts in our short-term memory at once
 }
